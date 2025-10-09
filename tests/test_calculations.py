6270a08934cd7cb230aa8ac85ee5a680148a8a93
Lekhana Sandra <80185991+SLekhana@users.noreply.github.com>
2025-10-09 03:23:15 -0400

    Update test_calculations.py

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,22 +1,31 @@
 import pytest
-from app.calculation import CalculationFactory
+from app.calculation.calculation import Calculation, CalculationFactory
+from app.operation import operations
 
-def test_factory_add_and_execute():
-    calc = CalculationFactory.create(1, 2, "add")
+def test_add():
+    calc = Calculation(1, 2, operations.add)
+    assert calc.perform() == 3
     assert calc.execute() == 3
 
-def test_factory_subtract_and_execute():
-    calc = CalculationFactory.create(10, 3, "subtract")
-    assert calc.execute() == 7
+def test_repr():
+    calc = Calculation(1, 2, operations.add)
+    assert "Calculation" in repr(calc)
 
-def test_factory_multiply_and_execute():
-    calc = CalculationFactory.create(3, 5, "multiply")
-    assert calc.execute() == 15
+def test_divide_by_zero():
+    calc = Calculation(1, 0, operations.divide)
+    with pytest.raises(ZeroDivisionError):
+        calc.perform()
 
-def test_factory_division_and_execute():
-    calc = CalculationFactory.create(20, 4, "divide")
-    assert calc.execute() == 5
+def test_factory_good_ops():
+    c = CalculationFactory.create(3, 4, "add")
+    assert isinstance(c, Calculation)
+    c = CalculationFactory.create(3, 4, operations.subtract)
+    assert isinstance(c, Calculation)
 
-def test_factory_invalid_operation():
+def test_factory_invalid_op_str():
     with pytest.raises(ValueError):
-        CalculationFactory.create(1,2,"pow")
+        CalculationFactory.create(1, 2, "not_a_real_op")  # This covers line 25
+
+def test_factory_invalid_op_type():
+    with pytest.raises(ValueError):
+        CalculationFactory.create(1, 2, None)  # This covers line 26
